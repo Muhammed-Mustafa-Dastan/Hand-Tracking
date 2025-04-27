6c90d29e79fc754a40fd337bbeb024740d0d7059
Muhammed-Mustafa-Dastan <89631701+Muhammed-Mustafa-Dastan@users.noreply.github.com>
2025-04-27 19:15:46 +0300

    Adjust FPS display font size in hand tracking module; add volume control script for webcam feed

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -50,7 +50,7 @@ def main():
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 2,
                     (255, 0, 255), 2)
         cv2.imshow("Image", img)
         cv2.waitKey(1)
