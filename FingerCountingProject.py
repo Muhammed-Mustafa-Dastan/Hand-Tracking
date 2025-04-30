ce6be631b911298e302cd55c2d50ab6c694861fd
Muhammed-Mustafa-Dastan <89631701+Muhammed-Mustafa-Dastan@users.noreply.github.com>
2025-04-30 23:07:56 +0300

    Implement hand detection and finger counting logic in main loop

--- FingerCountingProject.py
+++ FingerCountingProject.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import os
+import HandTrackingModule as htm
 
 wCam, hCam = 640, 480
 cap = cv2.VideoCapture(0)
@@ -15,8 +16,46 @@ for imgPath in myList:
     image = cv2.imread(f'{folderPath}/{imgPath}')
     overlayList.append(image)
 
+print(len(overlayList))
+pTime = 0
+
+detector = htm.handDetector(detectionCon=1, maxHands=2)
+
+tipIds = [4, 8, 12, 16, 20]
+
 while True:
     success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
+    
+    if len(lmList) != 0:
+        fingers = []
+        
+        # Thumb
+        if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        
+        # 4 Fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+
+    h, w, c = overlayList[0].shape
+    img[0:h, 0:w] = overlayList[0]
+    
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    
+    cv2.putText(img, f'FPS: {int(fps)}', (200, 40), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
